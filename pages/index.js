004b89d69fab38e3e81492264bbc64c634b6993e
Codere-Israel <uigyhiojppoji@@@jhfvhj1234>
2024-11-07 17:16:10 +0200

    start project

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Task Cost Calculator</h1>
+            <p>This is the Next.js version of your application!</p>
+        </div>
+    );
+}
